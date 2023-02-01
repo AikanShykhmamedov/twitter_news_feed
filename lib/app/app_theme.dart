c21e4794acc9437a599e894ac7b00ccfef5f0a9c
Aikan Shykhmamedov <aikan.shykhmamedov@gmail.com>
2023-02-02 03:37:19 +0400

    Update app_theme.dart

--- lib/app/app_theme.dart
+++ lib/app/app_theme.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 final appTheme = ThemeData(
+  scaffoldBackgroundColor: _colorScheme.surface,
   colorScheme: _colorScheme,
   appBarTheme: _appBarTheme,
+  dividerTheme: _dividerTheme,
+  textTheme: _textTheme,
 );
 
 const _colorScheme = ColorScheme(
@@ -11,6 +14,7 @@ const _colorScheme = ColorScheme(
   onPrimary: Color(0xffffffff),
   secondary: Color(0xff101418),
   onSecondary: Color(0xffffffff),
+  secondaryContainer: Color(0xff566370),
   error: Color(0xffe54e4e),
   onError: Color(0xffffffff),
   background: Color(0xffffffff),
@@ -28,7 +32,36 @@ final _appBarTheme = AppBarTheme(
     bottom: BorderSide(
       color: _colorScheme.outline,
       width: 0.0,
-    )
+    ),
   ),
   centerTitle: true,
+);
+
+final _dividerTheme = DividerThemeData(
+  color: _colorScheme.outline,
+  space: 1,
+  thickness: 0.0,
+);
+
+const _textTheme = TextTheme(
+  titleMedium: TextStyle(
+    fontFamily: 'HelveticaNeueLTPro',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  ),
+  bodyMedium: TextStyle(
+    color: Color(0xff101418),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  ),
+  bodySmall: TextStyle(
+    color: Color(0xff566370),
+    fontSize: 15,
+  ),
+  labelSmall: TextStyle(
+    color: Color(0xff566370),
+    fontSize: 12,
+    letterSpacing: 0.25,
+  ),
 );
