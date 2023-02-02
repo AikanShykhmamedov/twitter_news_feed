6606f44c3e2896e44f2097ba6d7582a508b65829
Aikan Shykhmamedov <aikan.shykhmamedov@gmail.com>
2023-02-02 15:15:15 +0400

    Update twitter_api package

--- packages/twitter_api/lib/src/models/user_tweets_response.dart
+++ packages/twitter_api/lib/src/models/user_tweets_response.dart
@@ -13,20 +13,21 @@ class UserTweetsResponse {
   final List<Tweet> tweets;
   final List<Media> media;
   final User user;
-  final String nextToken;
+  final String? nextToken;
 
   factory UserTweetsResponse.fromJson(Map<String, dynamic> json) {
     final data = (json['data'] as List).cast<Map<String, dynamic>>();
     final tweets = data.map(Tweet.fromJson).toList();
 
     final includes = (json['includes'] as Map).cast<String, dynamic>();
-    final mediaJson = (includes['media'] as List).cast<Map<String, dynamic>>();
-    final media = mediaJson.map(Media.fromJson).toList();
+    final mediaJson =
+        (includes['media'] as List?)?.cast<Map<String, dynamic>>();
+    final media = mediaJson?.map(Media.fromJson).toList() ?? [];
     final usersJson = (includes['users'] as List).cast<Map<String, dynamic>>();
     final users = usersJson.map(User.fromJson).toList();
 
     final meta = (json['meta'] as Map).cast<String, dynamic>();
-    final nextToken = meta['next_token'] as String;
+    final nextToken = meta['next_token'] as String?;
 
     return UserTweetsResponse(
       tweets: tweets,
--- packages/twitter_api/lib/twitter_api.dart
+++ packages/twitter_api/lib/twitter_api.dart
@@ -3,4 +3,5 @@ export 'src/models/media_type.dart';
 export 'src/models/public_metrics.dart';
 export 'src/models/tweet.dart';
 export 'src/models/user.dart';
+export 'src/models/user_tweets_response.dart';
 export 'src/twitter_api_client.dart';
